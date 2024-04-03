cfb12ca320c821cde6cbb983617c791345d4199a
sunithabalagolla <sunithabalagolla@gmail.com>
2024-04-03 14:23:23 +0530

    google logo

---
+++ src/GoogleLogo/Google.jsx
@@ -0,0 +1,13 @@
+import './Google.css';
+
+export function GoogleLogo(){
+    return(
+        <div className='google_logo'>
+        
+        
+        <div className='logo'>
+
+        </div>
+        </div>
+    )
+}
