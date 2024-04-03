23aee30efcb052c76d480480e060a852f3cdfae4
sunithabalagolla <sunithabalagolla@gmail.com>
2024-04-03 14:20:10 +0530

    git flip

---
+++ src/Responsive/Responsive.jsx
@@ -0,0 +1,26 @@
+import './Responsive.css';
+
+
+export function Responsive(){
+    return(
+        <>
+        <div className="main">
+
+            <div>1</div>
+            
+            <div>2</div>
+            
+            <div>3</div>
+            
+            <div>4</div>
+            
+            <div>5</div>
+            
+            <div>6</div>
+            
+           
+        </div>
+        
+        </>
+    )
+}
